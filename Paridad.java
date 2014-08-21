ea56f8a061219ade48ce6140318a2c9ea52f28df
WalterMartinez <waltermateo23@outlook.com>
2014-08-20 22:11:25 -0600

    Numero par o impar

---
+++ Paridad.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Paridad {
+
+	public static void main(String[] args) {
+		Scanner S = new Scanner(System.in); 
+		System.out.println("Por favor Ingrese Un Numero ");
+		int n; n = S.nextInt();
+		if(n%2==0){ System.out.println("El Numero Es Par"); }
+		else{ System.out.println("El Numero Es Impar"); }
+		
+
+}
+}
+		
